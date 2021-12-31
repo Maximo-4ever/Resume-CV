4507b6bc6f34ade345be5c2bf7e0459da76ace2e
Maximo-4ever <maximilianocm06@gmail.com>
2021-12-30 23:10:48 -0600

    permite descargar el documento HTML a PDF

---
+++ main.js
@@ -0,0 +1,14 @@
+const areaCv = document.getElementById("area-cv");
+const btnDownload = document.querySelector(".btnDownload");
+const opt = {
+  margin: 0,
+  filename: "Maximo 4ever - CV.pdf",
+  image: { type: "jpeg", quality: 1 },
+  enableLinks: true,
+  html2canvas: { scale: 3 },
+  jsPDF: { unit: "px", format: [1065, 1265], orientation: "portrait" },
+};
+
+btnDownload.addEventListener("click", () => {
+  html2pdf().set(opt).from(areaCv).save();
+});
